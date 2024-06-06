qyot27/ffms2_cplugin

commit 241b6473dca15b15eaf28a556a8c6bbba0e81bc1

Make it Linux friendly

src/avisynth_c/avs_lib.c
@@ -27,22 +27,47 @@
 #include "avs_common.h"
 
 ffms_avs_lib_t ffms_avs_lib;
-static volatile LONG ref = 0;
+static volatile int ref = 0;
+
+#if _WIN32
+#include <windows.h>
+#define avs_open() LoadLibraryW( L"avisynth" )
+#define avs_close FreeLibrary
+#define avs_address GetProcAddress
+#else
+#include <dlfcn.h>
+#if __APPLE__
+#define avs_open() dlopen( "libavisynth.dylib", RTLD_NOW )
+#else
+#define avs_open() dlopen( "libavisynth.so", RTLD_NOW )
+#endif
+#define avs_close dlclose
+#define avs_address dlsym
+#endif
 
 int ffms_load_avs_lib( AVS_ScriptEnvironment *env )
 {
-    if( InterlockedIncrement( &ref ) > 1 ) /* already initted - exit */
+#ifdef _WIN32
+    if (InterlockedIncrement(&ref) > 1) /* already initted - exit */
+#else
+    if (__atomic_add_fetch(&ref, 1, __ATOMIC_SEQ_CST))
+#endif // _WIN32
         return 0;
-    ffms_avs_lib.library = LoadLibrary("avisynth");
+
+    ffms_avs_lib.library = avs_open();
     if (!ffms_avs_lib.library)
         return -1;
 
 #define LOAD_AVS_FUNC(name, continue_on_fail)                  \
     ffms_avs_lib.name =                                        \
-         (void *)GetProcAddress(ffms_avs_lib.library, #name ); \
+         (void*)avs_address(ffms_avs_lib.library, #name);                \
     if( !continue_on_fail && !ffms_avs_lib.name )              \
         goto fail;
-    OutputDebugString( "FFMS2 avs plugin: Initializing..." );
+#ifdef _WIN32
+#ifdef _DEBUG
+    OutputDebugString("FFMS2 avs plugin: Initializing...");
+#endif
+#endif // _WIN32
 
     LOAD_AVS_FUNC( avs_add_function, 0 );
     LOAD_AVS_FUNC( avs_at_exit, 0 );
@@ -99,16 +124,20 @@ int ffms_load_avs_lib( AVS_ScriptEnvironment *env )
 
     return 0;
 fail:
-    ffms_free_avs_lib( NULL, NULL );
+    ffms_free_avs_lib();
     return -1;
 }
 
-void AVSC_CC ffms_free_avs_lib( void *user_data, AVS_ScriptEnvironment *env )
+void AVSC_CC ffms_free_avs_lib()
 {
     /* only free the memory if there are no more referencess */
-    if( !InterlockedDecrement( &ref ) && ffms_avs_lib.library )
+#ifdef _WIN32
+    if (!InterlockedDecrement(&ref) && ffms_avs_lib.library)
+#else
+    if (!__atomic_sub_fetch(&ref, 1, __ATOMIC_SEQ_CST) && ffms_avs_lib.library)
+#endif // _WIN32    
     {
         ffms_avs_lib.library = NULL;
-        free( ffms_avs_lib.library );
+        avs_close( ffms_avs_lib.library );
     }
 }

src/avisynth_c/avs_lib.h
@@ -89,6 +89,6 @@ typedef struct ffms_avs_lib_t
 /* it is highly convenient to have this library globally available to avoid having to pass it around */
 extern       ffms_avs_lib_t ffms_avs_lib;
 int          ffms_load_avs_lib( AVS_ScriptEnvironment *env );
-void AVSC_CC ffms_free_avs_lib( void *user_data, AVS_ScriptEnvironment *env );
+void AVSC_CC ffms_free_avs_lib();
 
 #endif

src/avisynth_c/avisynth.c
@@ -23,11 +23,8 @@
 #include "ff_filters.h"
 
 #ifdef MSVC
-#define strcasecmp _stricmp
 #include <assert.h>
 #define _Static_assert static_assert // for clang-cl at least 12.0
-#else
-#include <strings.h>
 #endif // MSVC
 
 #define MAX_CACHE_FILE_LENGTH 512 // Windows API should explode before getting this long
@@ -46,13 +43,6 @@ static int default_cache_file( const char *src, const char *user_cache_file, cha
         ret = !strcasecmp( src, user_cache_file );
     }
     return ret;
-}
-
-static int get_num_logical_cpus()
-{
-    SYSTEM_INFO SI;
-    GetSystemInfo( &SI );
-    return SI.dwNumberOfProcessors;
 }
 
 static AVS_Value AVSC_CC create_FFIndex( AVS_ScriptEnvironment *env, AVS_Value args, void *user_data )
@@ -153,13 +143,11 @@ static AVS_Value AVSC_CC create_FFVideoSource( AVS_ScriptEnvironment *env, AVS_V
         return avs_new_value_error( "FFVideoSource: No video track selected" );
     if( seek_mode < -1 || seek_mode > 3 )
         return avs_new_value_error( "FFVideoSource: Invalid seekmode selected" );
-    if( threads <= 0 )
-        threads = get_num_logical_cpus();
     if( rff_mode < 0 || rff_mode > 2 )
         return avs_new_value_error( "FFVideoSource: Invalid RFF mode selected" );
     if( rff_mode > 0 && fps_num > 0 )
         return avs_new_value_error( "FFVideoSource: RFF modes may not be combined with CFR conversion" );
-    if( !stricmp( src, timecodes ) )
+    if( !strcasecmp( src, timecodes ) )
         return avs_new_value_error( "FFVideoSource: Timecodes will overwrite the source" );
 
     char cache_file[MAX_CACHE_FILE_LENGTH];

src/avisynth_c/avs_common.h
@@ -21,12 +21,19 @@
 #ifndef FFAVSCOMMON_H
 #define FFAVSCOMMON_H
 
+#ifdef _WIN32
 #include <windows.h>
+#endif
 #undef EXTERN_C
 #define AVSC_NO_DECLSPEC
 #include "avisynth_c.h"
 #include "avs_lib.h"
 #include "avs_utils.h"
 #include "avs_convert.h"
+#ifdef MSVC
+#define strcasecmp _stricmp
+#else
+#include <strings.h>
+#endif // MSVC
 
 #endif

src/avisynth_c/avs_utils.c
@@ -30,125 +30,125 @@ enum AVPixelFormat csp_name_to_pix_fmt( const char *csp_name, enum AVPixelFormat
 {
     if( !csp_name || !strcmp( csp_name, "" ) )
         return def;
-    if( !stricmp( csp_name, "YV12" ) || !stricmp( csp_name, "YUV420P8" ) )
+    if( !strcasecmp( csp_name, "YV12" ) || !strcasecmp( csp_name, "YUV420P8" ) )
         return AV_PIX_FMT_YUV420P;
-    if( !stricmp( csp_name, "YUY2" ) )
+    if( !strcasecmp( csp_name, "YUY2" ) )
         return AV_PIX_FMT_YUYV422;
-    if( !stricmp( csp_name, "RGB24" ) )
+    if( !strcasecmp( csp_name, "RGB24" ) )
         return AV_PIX_FMT_BGR24;
-    if( !stricmp( csp_name, "RGB32" ) || !stricmp( csp_name, "RGBA32" ) )
+    if( !strcasecmp( csp_name, "RGB32" ) || !strcasecmp( csp_name, "RGBA32" ) )
         return AV_PIX_FMT_BGRA;
-    if( !stricmp( csp_name, "YV16" ) || !stricmp( csp_name, "YUV422P8" ) )
+    if( !strcasecmp( csp_name, "YV16" ) || !strcasecmp( csp_name, "YUV422P8" ) )
         return AV_PIX_FMT_YUV422P;
-    if( !stricmp( csp_name, "YV24" ) || !stricmp( csp_name, "YUV444P8" ) )
+    if( !strcasecmp( csp_name, "YV24" ) || !strcasecmp( csp_name, "YUV444P8" ) )
         return AV_PIX_FMT_YUV444P;
-    if( !stricmp( csp_name, "Y8" ) || !stricmp( csp_name, "GRAY" ) || !stricmp( csp_name, "GRAY8" ) )
+    if( !strcasecmp( csp_name, "Y8" ) || !strcasecmp( csp_name, "GRAY" ) || !strcasecmp( csp_name, "GRAY8" ) )
         return AV_PIX_FMT_GRAY8;
-    if( !stricmp( csp_name, "Y10" ) || !stricmp( csp_name, "GRAY10" ) )
+    if( !strcasecmp( csp_name, "Y10" ) || !strcasecmp( csp_name, "GRAY10" ) )
         return AV_PIX_FMT_GRAY10;
-    if( !stricmp( csp_name, "Y12" ) || !stricmp( csp_name, "GRAY12" ) )
+    if( !strcasecmp( csp_name, "Y12" ) || !strcasecmp( csp_name, "GRAY12" ) )
         return AV_PIX_FMT_GRAY12;
-    if( !stricmp( csp_name, "Y14" ) || !stricmp( csp_name, "GRAY14" ) )
+    if( !strcasecmp( csp_name, "Y14" ) || !strcasecmp( csp_name, "GRAY14" ) )
         return AV_PIX_FMT_GRAY14;
-    if( !stricmp( csp_name, "Y16" ) || !stricmp( csp_name, "GRAY16" ) )
+    if( !strcasecmp( csp_name, "Y16" ) || !strcasecmp( csp_name, "GRAY16" ) )
         return AV_PIX_FMT_GRAY16;
-    if( !stricmp( csp_name, "Y32" ) || !stricmp( csp_name, "GRAYF32" ) )
+    if( !strcasecmp( csp_name, "Y32" ) || !strcasecmp( csp_name, "GRAYF32" ) )
         return AV_PIX_FMT_GRAYF32;
-    if( !stricmp( csp_name, "YV411" ) || !stricmp( csp_name, "YUV411P8" ) )
+    if( !strcasecmp( csp_name, "YV411" ) || !strcasecmp( csp_name, "YUV411P8" ) )
         return AV_PIX_FMT_YUV411P;
-    if( !stricmp( csp_name, "RGB48" ) )
+    if( !strcasecmp( csp_name, "RGB48" ) )
         return AV_PIX_FMT_BGR48;
-    if( !stricmp( csp_name, "RGB64" ) || !stricmp( csp_name, "RGBA64" ) )
+    if( !strcasecmp( csp_name, "RGB64" ) || !strcasecmp( csp_name, "RGBA64" ) )
         return AV_PIX_FMT_BGRA64;
-    if( !stricmp( csp_name, "YUV420P10" ) )
+    if( !strcasecmp( csp_name, "YUV420P10" ) )
         return AV_PIX_FMT_YUV420P10;
-    if( !stricmp( csp_name, "YUV422P10" ) )
+    if( !strcasecmp( csp_name, "YUV422P10" ) )
         return AV_PIX_FMT_YUV422P10;
-    if( !stricmp( csp_name, "YUV444P10" ) )
+    if( !strcasecmp( csp_name, "YUV444P10" ) )
         return AV_PIX_FMT_YUV444P10;
-    if( !stricmp( csp_name, "YUV420P12" ) )
+    if( !strcasecmp( csp_name, "YUV420P12" ) )
         return AV_PIX_FMT_YUV420P12;
-    if( !stricmp( csp_name, "YUV422P12" ) )
+    if( !strcasecmp( csp_name, "YUV422P12" ) )
         return AV_PIX_FMT_YUV422P12;
-    if( !stricmp( csp_name, "YUV444P12" ) )
+    if( !strcasecmp( csp_name, "YUV444P12" ) )
         return AV_PIX_FMT_YUV444P12;
-    if( !stricmp( csp_name, "YUV420P14" ) )
+    if( !strcasecmp( csp_name, "YUV420P14" ) )
         return AV_PIX_FMT_YUV420P14;
-    if( !stricmp( csp_name, "YUV422P14" ) )
+    if( !strcasecmp( csp_name, "YUV422P14" ) )
         return AV_PIX_FMT_YUV422P14;
-    if( !stricmp( csp_name, "YUV444P14" ) )
+    if( !strcasecmp( csp_name, "YUV444P14" ) )
         return AV_PIX_FMT_YUV444P14;
-    if( !stricmp( csp_name, "YUV420P16" ) )
+    if( !strcasecmp( csp_name, "YUV420P16" ) )
         return AV_PIX_FMT_YUV420P16;
-    if( !stricmp( csp_name, "YUV422P16" ) )
+    if( !strcasecmp( csp_name, "YUV422P16" ) )
         return AV_PIX_FMT_YUV422P16;
-    if( !stricmp( csp_name, "YUV444P16" ) )
+    if( !strcasecmp( csp_name, "YUV444P16" ) )
         return AV_PIX_FMT_YUV444P16;
-//    if( !stricmp( csp_name, "YUV420PS" ) )
+//    if( !strcasecmp( csp_name, "YUV420PS" ) )
 //        return AV_PIX_FMT_YUV420PF32;
-//    if( !stricmp( csp_name, "YUV422PS" ) )
+//    if( !strcasecmp( csp_name, "YUV422PS" ) )
 //        return AV_PIX_FMT_YUV422PF32;
-//    if( !stricmp( csp_name, "YUV444PS" ) )
+//    if( !strcasecmp( csp_name, "YUV444PS" ) )
 //        return AV_PIX_FMT_YUV444PF32;
-    if( !stricmp( csp_name, "YUVA420P" ) )
+    if( !strcasecmp( csp_name, "YUVA420P" ) )
         return AV_PIX_FMT_YUVA420P;
-    if( !stricmp( csp_name, "YUVA422P" ) )
+    if( !strcasecmp( csp_name, "YUVA422P" ) )
         return AV_PIX_FMT_YUVA422P;
-    if( !stricmp( csp_name, "YUVA444P" ) )
+    if( !strcasecmp( csp_name, "YUVA444P" ) )
         return AV_PIX_FMT_YUVA444P;
-    if( !stricmp( csp_name, "YUVA420P10" ) )
+    if( !strcasecmp( csp_name, "YUVA420P10" ) )
         return AV_PIX_FMT_YUVA420P10;
-    if( !stricmp( csp_name, "YUVA422P10" ) )
+    if( !strcasecmp( csp_name, "YUVA422P10" ) )
         return AV_PIX_FMT_YUVA422P10;
-    if( !stricmp( csp_name, "YUVA444P10" ) )
+    if( !strcasecmp( csp_name, "YUVA444P10" ) )
         return AV_PIX_FMT_YUVA444P10;
-//    if( !stricmp( csp_name, "YUVA420P12" ) )
+//    if( !strcasecmp( csp_name, "YUVA420P12" ) )
 //        return AV_PIX_FMT_YUVA420P12;
-    if( !stricmp( csp_name, "YUVA422P12" ) )
+    if( !strcasecmp( csp_name, "YUVA422P12" ) )
         return AV_PIX_FMT_YUVA422P12;
-//    if( !stricmp( csp_name, "YUVA444P12" ) )
+//    if( !strcasecmp( csp_name, "YUVA444P12" ) )
 //        return AV_PIX_FMT_YUVA444P12;
-//    if( !stricmp( csp_name, "YUVA420P14" ) )
+//    if( !strcasecmp( csp_name, "YUVA420P14" ) )
 //        return AV_PIX_FMT_YUVA420P14;
-//    if( !stricmp( csp_name, "YUVA422P14" ) )
+//    if( !strcasecmp( csp_name, "YUVA422P14" ) )
 //        return AV_PIX_FMT_YUVA422P14;
-//    if( !stricmp( csp_name, "YUVA444P14" ) )
+//    if( !strcasecmp( csp_name, "YUVA444P14" ) )
 //        return AV_PIX_FMT_YUVA444P14;
-    if( !stricmp( csp_name, "YUVA420P16" ) )
+    if( !strcasecmp( csp_name, "YUVA420P16" ) )
         return AV_PIX_FMT_YUVA420P16;
-    if( !stricmp( csp_name, "YUVA422P16" ) )
+    if( !strcasecmp( csp_name, "YUVA422P16" ) )
         return AV_PIX_FMT_YUVA422P16;
-    if( !stricmp( csp_name, "YUVA444P16" ) )
+    if( !strcasecmp( csp_name, "YUVA444P16" ) )
         return AV_PIX_FMT_YUVA444P16;
-//    if( !stricmp( csp_name, "YUVA420PS" ) )
+//    if( !strcasecmp( csp_name, "YUVA420PS" ) )
 //        return AV_PIX_FMT_YUVA420PF32;
-//    if( !stricmp( csp_name, "YUVA422PS" ) )
+//    if( !strcasecmp( csp_name, "YUVA422PS" ) )
 //        return AV_PIX_FMT_YUVA422PF32;
-//    if( !stricmp( csp_name, "YUVA444PS" ) )
+//    if( !strcasecmp( csp_name, "YUVA444PS" ) )
 //        return AV_PIX_FMT_YUVA444PF32;
-    if( !stricmp( csp_name, "RGBP" ) )
+    if( !strcasecmp( csp_name, "RGBP" ) )
         return AV_PIX_FMT_GBRP;
-    if( !stricmp( csp_name, "RGBP10" ) )
+    if( !strcasecmp( csp_name, "RGBP10" ) )
         return AV_PIX_FMT_GBRP10;
-    if( !stricmp( csp_name, "RGBP12" ) )
+    if( !strcasecmp( csp_name, "RGBP12" ) )
         return AV_PIX_FMT_GBRP12;
-    if( !stricmp( csp_name, "RGBP14" ) )
+    if( !strcasecmp( csp_name, "RGBP14" ) )
         return AV_PIX_FMT_GBRP14;
-    if( !stricmp( csp_name, "RGBP16" ) )
+    if( !strcasecmp( csp_name, "RGBP16" ) )
         return AV_PIX_FMT_GBRP16;
-    if( !stricmp( csp_name, "RGBPS" ) )
+    if( !strcasecmp( csp_name, "RGBPS" ) )
         return AV_PIX_FMT_GBRAPF32;
-    if( !stricmp( csp_name, "RGBAP" ) )
+    if( !strcasecmp( csp_name, "RGBAP" ) )
         return AV_PIX_FMT_GBRAP;
-    if( !stricmp( csp_name, "RGBAP10" ) )
+    if( !strcasecmp( csp_name, "RGBAP10" ) )
         return AV_PIX_FMT_GBRAP10;
-    if( !stricmp( csp_name, "RGBAP12" ) )
+    if( !strcasecmp( csp_name, "RGBAP12" ) )
         return AV_PIX_FMT_GBRAP12;
-//    if( !stricmp( csp_name, "RGBAP14" ) )
+//    if( !strcasecmp( csp_name, "RGBAP14" ) )
 //        return AV_PIX_FMT_GBRAP14;
-    if( !stricmp( csp_name, "RGBAP16" ) )
+    if( !strcasecmp( csp_name, "RGBAP16" ) )
         return AV_PIX_FMT_GBRAP16;
-    if( !stricmp( csp_name, "RGBAPS" ) )
+    if( !strcasecmp( csp_name, "RGBAPS" ) )
         return AV_PIX_FMT_GBRAPF32;
     return AV_PIX_FMT_NONE;
 }
@@ -281,27 +281,27 @@ enum AVPixelFormat vi_to_pix_fmt( const AVS_VideoInfo *vi )
 
 int resizer_name_to_swscale_name( const char *resizer )
 {
-    if( !stricmp( resizer, "FAST_BILINEAR" ) )
+    if( !strcasecmp( resizer, "FAST_BILINEAR" ) )
         return SWS_FAST_BILINEAR;
-    if( !stricmp( resizer, "BILINEAR" ) )
+    if( !strcasecmp( resizer, "BILINEAR" ) )
         return SWS_BILINEAR;
-    if( !stricmp( resizer, "BICUBIC" ) )
+    if( !strcasecmp( resizer, "BICUBIC" ) )
         return SWS_BICUBIC;
-    if( !stricmp( resizer, "X" ) )
+    if( !strcasecmp( resizer, "X" ) )
         return SWS_X;
-    if( !stricmp( resizer, "POINT" ) )
+    if( !strcasecmp( resizer, "POINT" ) )
         return SWS_POINT;
-    if( !stricmp( resizer, "AREA" ) )
+    if( !strcasecmp( resizer, "AREA" ) )
         return SWS_AREA;
-    if( !stricmp( resizer, "BICUBLIN" ) )
+    if( !strcasecmp( resizer, "BICUBLIN" ) )
         return SWS_BICUBLIN;
-    if( !stricmp( resizer, "GAUSS" ) )
+    if( !strcasecmp( resizer, "GAUSS" ) )
         return SWS_GAUSS;
-    if( !stricmp( resizer, "SINC" ) )
+    if( !strcasecmp( resizer, "SINC" ) )
         return SWS_SINC;
-    if( !stricmp( resizer, "LANCZOS" ) )
+    if( !strcasecmp( resizer, "LANCZOS" ) )
         return SWS_LANCZOS;
-    if( !stricmp( resizer, "SPLINE" ) )
+    if( !strcasecmp( resizer, "SPLINE" ) )
         return SWS_SPLINE;
     return 0;
 }

src/avisynth_c/ff_audsource.c
@@ -47,7 +47,7 @@ static int AVSC_CC get_parity( AVS_FilterInfo *fi, int n )
     return 0;
 }
 
-static int AVSC_CC get_audio( AVS_FilterInfo *fi, void *buf, INT64 start, INT64 count )
+static int AVSC_CC get_audio( AVS_FilterInfo *fi, void *buf, int64_t start, int64_t count )
 {
     ffaudiosource_filter_t *filter = fi->user_data;
     init_ErrorInfo( ei );

src/avisynth_c/ff_vidsource.c
@@ -223,7 +223,7 @@ static int AVSC_CC get_parity( AVS_FilterInfo *fi, int n )
     return fi->vi.image_type == AVS_IT_TFF;
 }
 
-static int AVSC_CC get_audio( AVS_FilterInfo *fi, void *buf, INT64 start, INT64 count )
+static int AVSC_CC get_audio( AVS_FilterInfo *fi, void *buf, int64_t start, int64_t count )
 {
     return 0;
 }
@@ -344,12 +344,8 @@ static AVS_Value init_output_format( ffvideosource_filter_t *filter, int dst_wid
     enum AVPixelFormat pix_fmt = frame->ConvertedPixelFormat;
 
     /* Detect whether we're using AviSynth 2.6 or AviSynth+ by
-     * looking for whether avs_is_planar_rgb exists. */
-    int avsplus;
-    if (GetProcAddress(ffms_avs_lib.library, "avs_is_planar_rgb") == NULL)
-        avsplus = 0;
-    else
-        avsplus = 1;
+     * looking for whether ExtractY exists. */
+    const int avsplus = ffms_avs_lib.avs_function_exists(filter->fi->env, "ExtractY");
 
     if( pix_fmt == AV_PIX_FMT_YUVJ420P || pix_fmt == AV_PIX_FMT_YUV420P )
         filter->fi->vi.pixel_type = AVS_CS_I420;

commit b559b92d4bec9dc1b9f4c0a1ada478b8621ac530

Fix avs lib release (regression from 241b6473dca15b15eaf28a556a8c6bbba0e81bc1)

src/avisynth_c/avs_lib.c
@@ -124,11 +124,11 @@ int ffms_load_avs_lib( AVS_ScriptEnvironment *env )
 
     return 0;
 fail:
-    ffms_free_avs_lib();
+    ffms_free_avs_lib(NULL, NULL);
     return -1;
 }
 
-void AVSC_CC ffms_free_avs_lib()
+void AVSC_CC ffms_free_avs_lib(void* user_data, AVS_ScriptEnvironment* env)
 {
     /* only free the memory if there are no more referencess */
     InterlockedDecrement(&ref);

src/avisynth_c/avs_lib.h
@@ -90,6 +90,6 @@ typedef struct ffms_avs_lib_t
 /* it is highly convenient to have this library globally available to avoid having to pass it around */
 extern       ffms_avs_lib_t ffms_avs_lib;
 int          ffms_load_avs_lib( AVS_ScriptEnvironment *env );
-void AVSC_CC ffms_free_avs_lib();
+void AVSC_CC ffms_free_avs_lib(void* user_data, AVS_ScriptEnvironment* env);
 
 #endif